ebe15af61e008a05c5d7c4ae7ff7b79418e6593d
yanis26x <yanis.djenadi@hotmail.com>
2026-03-14 09:06:08 -0400

    ajout bouton pr changer le bg

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,12 +1,32 @@
 import { useNavigate } from "react-router-dom";
 import "./home.css";
+import { useState } from "react";
 
 export default function Home() {
   const navigate = useNavigate();
+  const backgrounds = [
+    "/bluePurpleBackground.jpg",
+    "/SpotifyLogoRed.webp"
+  ];
 
+    const [bgIndex, setBgIndex] = useState(0);
+
+      function changeBackground() {
+    setBgIndex((prev) => (prev === 0 ? 1 : 0));
+  }
   return (
-    <div className="homeDiv">
-
+    <div className="homeDiv" style={{ backgroundImage: `url(${backgrounds[bgIndex]})` }}>
+      
+<button
+  className="BTNchangeBg"
+  onClick={changeBackground}
+  style={{
+    // a changer !!!!
+    backgroundImage: `url(${backgrounds[bgIndex === 0 ? 1 : 0]})` 
+  }}
+>
+  light/dark
+</button>
 
       <button className="BTNvendreTop" onClick={() => navigate("/vendreParfum")}>
   <span>Vendre</span>
